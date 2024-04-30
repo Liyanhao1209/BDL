4f7b6ea0c871a25ce7a550bd7c1083e2ad8cede5
unknown <3054117680@qq.com>
2024-04-30 15:17:44 +0800

    lab1:add multi-business heatmap

--- matlab_labs/Data_visualization/util/region_sum.m
+++ matlab_labs/Data_visualization/util/region_sum.m
@@ -1,10 +1,12 @@
-function [] = region(business,time_id,sx,ex,sy,ey,data,target_dir)
-    arr = data(business,1:10000,time_id);
+function [] = region_sum(time_id,sx,ex,sy,ey,data,target_dir)
+    arr = data(:,1:10000,time_id);
     city = zeros(100,100);
     
     for i=1:100
         for j=1:100
-            city(i,j) = arr( (i-1)*100+j);
+            for k = 1:5
+                city(i,j) = city(i,j)+ arr( k,(i-1)*100+j);
+            end
         end
     end
     
@@ -21,7 +23,7 @@ function [] = region(business,time_id,sx,ex,sy,ey,data,target_dir)
     figure
     h = heatmap(mat);
     
-    title(sprintf('第%d业务第%d时间段流量热力图',business,time_id))
+    title(sprintf('所有第%d时间段流量热力图',time_id))
     xlabel(sprintf('横向小区号(自%d开始)',sx))
     ylabel(sprintf('纵向小区号(自%d开始)',sy))
     
@@ -30,7 +32,7 @@ function [] = region(business,time_id,sx,ex,sy,ey,data,target_dir)
     
     grid on
     
-    png_name = sprintf('%d_%d_%d_%d %d_%d.png',business,time_id,sx,ex,sy,ey);
+    png_name = sprintf('%d_%d_%d %d_%d.png',time_id,sx,ex,sy,ey);
     filename = fullfile(target_dir,png_name);
     exportgraphics(gcf,filename,'Resolution',1024)
     
